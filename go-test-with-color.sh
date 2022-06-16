bf2213664e0a7ae39907aff8505c4f44e553e4a2
James Shieh <jamesshieh0510@gmail.com>
2022-06-16 10:26:53 +0800

    Update go-test-with-color.sh

--- go-test-with-color.sh
+++ go-test-with-color.sh
@@ -1,1 +1,1 @@
-go test -v ./... | sed ''/PASS/s//$(printf "\033[32mPASS\033[0m")/'' | sed ''/FAIL/s//$(printf "\033[31mFAIL\033[0m")/''
+go test -v ./... | sed ''/PASS/s//$(printf "\033[32mPASS\033[0m")/'' | sed ''/FAIL/s//$(printf "\033[31mFAIL\033[0m")/'' | sed ''/INFO/s//$(printf "\033[33mINFO\033[0m")/''
