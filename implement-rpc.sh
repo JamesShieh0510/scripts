584073942e5225ccb4f2d47f29326495d30da517
James Shieh <jamesshieh0510@gmail.com>
2022-06-17 13:02:55 +0800

    Update implement-rpc.sh

--- implement-rpc.sh
+++ implement-rpc.sh
@@ -1,10 +1,10 @@
-#!/bin/sh
+#!/bin/bash
 
 text=$(cat ./*/*.proto */*/*.proto 2>/dev/null | sed -rn 's/[[:space:]]+rpc [a-zA-Z]+\((.*)\) returns \((.*)\) \{\};/message \1 {\nmessage \2 {/p'
 )
 
 #The $'...' construct expands embedded ANSI escape sequences.
-echo $"$text" > temp
+echo $"$text" > temp.txt
 output=""
 while read line; do
   target=$(cat ./*/*.proto */*/*.proto 2>/dev/null | grep "$line")
@@ -12,6 +12,6 @@ while read line; do
   then
     output="$output\n$line\n\t\n}"
   fi
-done < temp
-echo $output
-rm temp
+done < temp.txt
+printf "$output\n"
+rm temp.txt
