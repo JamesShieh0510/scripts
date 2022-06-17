e4afd0bc961225f882f99e3cdec3ca451544f180
James Shieh <jamesshieh0510@gmail.com>
2022-06-17 12:18:56 +0800

    Update implement-rpc.sh

--- implement-rpc.sh
+++ implement-rpc.sh
@@ -1,19 +1,16 @@
 #!/bin/sh
 
-echo "//generate from script......"
+echo "//generate from the script......"
 text=$(cat ./*/*.proto */*/*.proto 2>/dev/null | sed -rn 's/[[:space:]]+rpc [a-zA-Z]+\((.*)\) returns \((.*)\) \{\};/message \1 {\nmessage \2 {/p'
 )
 echo $text > temp
 output=""
-cat temp | while read line 
-do
+while read line; do
   target=$(cat ./*/*.proto */*/*.proto 2>/dev/null | grep "$line") 
-  echo "target:$target, line:$line"
   if [ -z $target ]
   then
     output="$output\n$line\n\t\n}"
   fi
-done
+done < temp
 echo $output
 rm temp
-echo "end"
