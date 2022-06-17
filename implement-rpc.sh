165b572ecc258e03840f8814693c770a0fe39e04
James Shieh <jamesshieh0510@gmail.com>
2022-06-17 12:00:22 +0800

    Update implement-rpc.sh

--- implement-rpc.sh
+++ implement-rpc.sh
@@ -1,1 +1,15 @@
-cat ./*/*.proto */*/*.proto 2>/dev/null | sed -rn 's/[[:space:]]+rpc [a-zA-Z]+\((.*)\) returns \((.*)\) \{\};/message \1 {\n}\nmessage \2 {\n}/p'
+text=$(cat ./*/*.proto */*/*.proto 2>/dev/null | sed -rn 's/[[:space:]]+rpc [a-zA-Z]+\((.*)\) returns \((.*)\) \{\};/message \1 {\nmessage \2 {/p'
+)
+echo $text > temp
+output=""
+cat temp | while read line 
+do
+  target=$(cat ./*/*.proto */*/*.proto 2>/dev/null | grep "$line") 
+  if [ -z $target ]
+  then
+    output="$output\n$line\n\t\n}"
+  fi
+done
+echo $output
+rm temp
+
