2b52ed396e81fb46f566f2874d65d4d595e4046c
James Shieh <jamesshieh0510@gmail.com>
2022-08-12 09:25:36 +0800

    Update get-go-mod-version.sh

--- get-go-mod-version.sh
+++ get-go-mod-version.sh
@@ -1,3 +1,3 @@
 
-echo v0.0.0-$(TZ=UTC0 git show -s --date=iso-local --date=format-local:%Y%m%d%H%M%S --format=%cd $(git rev-parse HEAD | egrep -oh '^[0-9a-z]{12}' | awk 'NR==1{print $1}'))-$(git rev-parse HEAD | egrep -oh '^[0-9a-z]{12}')
+echo v0.0.0-$(TZ=UTC0 git show -s --date=iso-local --date=format-local:%Y%m%d%H%M%S --format=%cd $(git rev-parse HEAD | egrep -oh '^[0-9a-z]{12}' | awk 'NR==1{print $1}'))-$(git rev-parse HEAD | egrep -oh '^[0-9a-z]{12}' | awk 'NR==1{print $1}')
 
