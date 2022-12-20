c08726f7eb7181047c8c4acbd94557ac8855ee95
James Shieh <jamesshieh0510@gmail.com>
2022-12-20 21:17:45 +0800

    Create check-the-dependence-by-module-name.sh

---
+++ check-the-dependence-by-module-name.sh
@@ -0,0 +1,17 @@
+target_module=$1
+if [ -z $target_module ]
+then
+    echo "Please input the module name you want to check (ex:s-user): "
+    read env
+fi
+go mod tidy; 
+version=$(cat go.mod | grep $target_module | awk '{print $2}');
+
+module=$(go mod graph | grep " .*$target_module@$version" | awk '{print $1}')
+
+echo "Which modules determined the version of $target_module :\n\n"
+printf "\033[32m$module\033[0m"
+echo "\n\n----------------------------------\n"
+
+echo "all the modules that depend on $target_module are:\n\n"
+go mod graph | grep " .*$target_module"
