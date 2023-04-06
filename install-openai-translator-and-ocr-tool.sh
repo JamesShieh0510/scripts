803cc91d4f925c9820dd51acb77cbe7cf3e88258
James Shieh <jamesshieh0510@gmail.com>
2023-04-06 12:41:18 +0800

    Update install-openai-translator-and-ocr-tool.sh

--- install-openai-translator-and-ocr-tool.sh
+++ install-openai-translator-and-ocr-tool.sh
@@ -1,4 +1,8 @@
 #bash <(curl -s https://raw.githubusercontent.com/JamesShieh0510/scripts/master/install-openai-translator-and-ocr-tool.sh)
+# add shortcut: 
+# 1. https://www.icloud.com/shortcuts/fa91687e481849d6a27ff873ec71599b
+# 2. https://www.icloud.com/shortcuts/14d11971215f4d1bb20a6a8fd3bb3daa
+
 curl -O https://files.littlebird.com.au/ocr2.zip
 unzip ocr2.zip
 sudo cp ocr /usr/local/bin
